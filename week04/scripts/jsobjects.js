d61327a631abb2e003fcc26078c8a395f9bf6297
Karimasapliaciones <karim.valenzuela@asaplicacion.com>
2025-02-04 14:01:23 -0700

    w04 learning activities

---
+++ week04/scripts/jsobjects.js
@@ -0,0 +1,49 @@
+// courses.js
+const aCourse = {
+    code: "CSE121b",
+    name:"Javascript Language",
+    sections:[
+    {sectionNum:1, roomNum:'STC 353', enrolled:26 , days:'TTh',instructor:'Bro T'},
+    {sectionNum:2, roomNum:'STC 347', enrolled:28 , days:'TTh',instructor:'Sis A'}
+    ],
+    changeEnrollment: function (sectionNum, add = true){
+        // find the right section...Array.findIndex will work here
+        const sectionIndex = this.sections.findIndex(
+            (section) => section.sectionNum == sectionNum
+        )
+        if (sectionIndex >= 0){
+            if (add){
+                this.sections[sectionIndex].enrolled++
+            } else {
+                this.sections[sectionIndex].enrolled--
+            }
+            renderSections(this.sections)
+        }
+    }
+}
+
+
+
+function setCourseInfo(course){
+    const courseName = document.querySelector("#courseName")
+    const courseCode = document.querySelector("#courseCode")
+    courseName.textContent = course.name
+    course.textContent  = course.code
+}
+
+function sectionTemplate(section){
+    return `<tr>
+    <td>${section.sectionNum}</td>
+    <td>${section.roomNum}</td>
+    <td>${section.enrolled}</td>
+    <td>${section.days}</td>
+    <td>${section.instructor}</td></tr>`
+}
+
+function renderSections(sections){
+    const html = sections.map(sectionTemplate)
+    document.querySelector("#sections").innerHTML = html.join("")
+}
+
+setCourseInfo(aCourse)
+renderSections(aCourse.sections)
---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,11 @@
+// select elements for the footer dates
+const year = document.getElementById('currentyear')
+const modified = document.querySelector("#lastModified")
+
+
+// use the date object
+const today = new Date()
+
+year.innerHTML = `© ${today.getFullYear()}`
+
+modified.innerHTML = `Last modified: ${document.lastModified}`
